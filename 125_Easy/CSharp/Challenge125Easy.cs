b75f06f36950c0158379e5df288e37b87abd05f7
unknown <mattlorimor@gmail.com>
2015-09-10 23:19:10 -0500

    Renamed the challenge directories.

---
+++ 125_Easy/CSharp/Challenge125Easy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+    class Challenge125Easy
+    {
+        static void Main(string[] args)
+        {
+            var test = File.ReadAllLines(@"./InputText.txt");
+            int wordCount = 0;
+            foreach (var line in test)
+            {
+                var trimmedLine = line.Trim();
+                if (!trimmedLine.Equals(String.Empty))
+                {
+                    var splitLine = line.Split(' ')
+                        .ToList();
+                    splitLine.RemoveAll(x => x == string.Empty);
+                    wordCount += splitLine.Count();
+                }
+            }
+        }
+    }
+}
